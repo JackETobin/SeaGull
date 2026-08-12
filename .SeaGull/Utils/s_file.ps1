cbd7f50288f838f5b3107e56366bbb2d37d64002
Jack <127067654+JackETobin@users.noreply.github.com>
2026-08-12 21:45:09 +0900

    Add files via upload

---
+++ .SeaGull/Utils/s_file.ps1
@@ -0,0 +1,78 @@
+function File_Process_Out {
+    [OutputType([string[]])]
+    param( [string[]] $raw_In )
+    0..($raw_In.Count - 2) | ForEach-Object {
+        if($_ -lt 0) { break }
+        $raw_In[$_] = $raw_In[$_] + ";"
+    }
+    return $raw_In
+}
+
+function File_Write {
+    param( 
+        [string] $dir_In,
+        [string[]] $content_In
+    )
+    if((Test-Path -Path $dir_In) -ne $true) {
+        if($Settings.verbose -eq $true) { Write-Host ("Unable to find directory: " + $dir_In) }
+        return $false
+    }
+    $write = File_Process_Out -raw_In $content_In
+    Set-Content -NoNewline -Path $dir_In -Value $write
+}
+
+function File_Read {
+    [OutputType([string[]])]
+    param( [string] $dir_In )
+    if((Test-Path -Path $dir_In) -ne $true) {
+        if($Settings.verbose -eq $true) { Write-Host ("Unable to find directory: " + $dir_In) }
+        return $null
+    }
+    $content = Get-Content -Raw -Path $dir_In
+    if($content) { $content = $content.Split(";") }
+    return $content
+}
+
+function File_Make {
+    [OutputType([bool])]
+    param( [string] $dir_In )
+    if((Test-Path -Path $dir_In) -eq $true) {
+        return $true
+    }
+    $null = New-Item -ItemType "file" -Path $dir_In
+    return (Test-Path -Path $dir_In)
+}
+function File_Kill {
+    [OutputType([bool])]
+    param( [string] $dir_In )
+    Remove-Item -Path $dir_In
+    return !(Test-Path $dir_In)
+}
+
+function File_SetBlock {
+    [OutputType([bool])]
+    param( [string] $name_In )
+    $dir = ("./.SeaGull/Temp/Wait/" + $name_In)
+    $null = New-Item -ItemType "file" -Path $dir
+    return (Test-Path -Path $dir)
+}
+
+function File_Unblock {
+    [OutputType([bool])]
+    param( [string] $name_In )
+    $dir = ("./.SeaGull/Temp/Wait/" + $name_In)
+    $null = Remove-Item -Path $dir
+    return !(Test-Path -Path $dir)
+}
+
+function File_WaitBlock {
+    [OutputType([bool])]
+    param( [string[]] $names_In )
+    ForEach($name in $names_In) {
+        $dir = ("./.SeaGull/Temp/Wait/" + $name)
+        while((Test-Path -Path $dir -PathType Leaf) -eq $true) {
+            Start-Sleep -Milliseconds 10
+        }
+    }
+    return
+}
